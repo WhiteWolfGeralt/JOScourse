2dc2d80ee47ca8c481d428089c3d5063302a0c95
Daniil Subbotin <danya.subbotin34@gmail.com>
2021-12-30 01:29:57 +0300

    lab 10 done

--- kern/env.c
+++ kern/env.c
@@ -337,6 +337,9 @@ env_create(uint8_t *binary, size_t size, enum EnvType type) {
     if (env_alloc(&env, 0, type) < 0) {
         panic("It's impossible to allocate an env.\n");
     }
+    if (type == ENV_TYPE_FS) {
+        env->env_tf.tf_rflags |= FL_IOPL_3;
+    }
     env->binary = binary;
     load_icode(env, binary, size);
 }
--- kern/syscall.c
+++ kern/syscall.c
@@ -359,7 +359,12 @@ sys_ipc_recv(uintptr_t dstva, uintptr_t maxsize) {
 static int
 sys_region_refs(uintptr_t addr, size_t size, uintptr_t addr2, uintptr_t size2) {
     // LAB 10: Your code here
-    return 0;
+    int max_ref = region_maxref(current_space, addr, size);
+    if (addr2 >= MAX_USER_ADDRESS)
+        return max_ref;
+
+    int max_ref2 = region_maxref(current_space, addr2, size2);
+    return max_ref - max_ref2;
 }
 
 /* Dispatches to the correct kernel function, passing the arguments. */
